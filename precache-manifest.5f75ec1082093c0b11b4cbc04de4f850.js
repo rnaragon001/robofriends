071ffb446e080e619a2372eaaf05cc07ee5682b5
Rod Aragon <rnaragon001@yahoo.com>
2019-03-20 21:14:16 +0800

    Updates

---
+++ precache-manifest.5f75ec1082093c0b11b4cbc04de4f850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "82f321f78832f84e9b85",
+    "url": "/robofriends/static/js/main.82f321f7.chunk.js"
+  },
+  {
+    "revision": "a606bb6373f90a86a698",
+    "url": "/robofriends/static/js/1.a606bb63.chunk.js"
+  },
+  {
+    "revision": "82f321f78832f84e9b85",
+    "url": "/robofriends/static/css/main.ab270065.chunk.css"
+  },
+  {
+    "revision": "a606bb6373f90a86a698",
+    "url": "/robofriends/static/css/1.52c45e5d.chunk.css"
+  },
+  {
+    "revision": "6427280f26ae68761081b8b32b71d1fc",
+    "url": "/robofriends/index.html"
+  }
+];
